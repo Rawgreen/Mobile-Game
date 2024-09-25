76b365bbc3ab0219a33add58d74512e8b037b93f
Rawgreen <mhali.varol@gmail.com>
2024-09-25 20:14:09 +0300

    25.09.24
    upgrade system is nearly done, now value to be increased is displayed on the button, redundancy code in script is cleared, lambda expressions and delegation used to perform upgrade, upgrade cost will implemented next.

--- Assets/Scripts/Miscellaneous/ButtonManager.cs
+++ Assets/Scripts/Miscellaneous/ButtonManager.cs
@@ -3,18 +3,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ButtonManager : MonoBehaviour
 {
-    private CannonManager cannonManager;
     private CannonStats cannonStats;
 
-    private Button damageButton;
+    private Dictionary<string, Button> buttons = new Dictionary<string, Button>();
+    private Dictionary<string, TextMeshProUGUI> buttonTexts = new Dictionary<string, TextMeshProUGUI>();
+    private Dictionary<string, float> upgradeAmounts = new Dictionary<string, float>();
 
     private void Start()
     {
         cannonStats = CannonManager.Instance.GetCannonStats();
-        damageButton = GameObject.Find("DamageUpgrade").GetComponent<Button>();
+
+        InitializeButton("DamageUpgrade", 1f, 5f);
+        InitializeButton("HealthUpgrade", 1f, 5f);
+        InitializeButton("RadiusUpgrade", 0.5f, 2.5f);
+        InitializeButton("ProjectileVelocityUpgrade", 0.1f, 1.0f);
+        InitializeButton("ShootingRateUpgrade", 0.1f, 0.5f);
+    }
+
+    private void InitializeButton(string buttonName, float minValue, float maxValue)
+    {
+        Button button = GameObject.Find(buttonName).GetComponent<Button>();
+        buttons[buttonName] = button;
+
+        TextMeshProUGUI buttonText = GetTextComponent(button);
+        buttonTexts[buttonName] = buttonText;
+
+        float upgradeAmount = GenerateRandomValue(minValue, maxValue);
+        upgradeAmounts[buttonName] = upgradeAmount;
+
+        UpdateValueText(buttonName, buttonText, upgradeAmount);
     }
 
     private float GenerateRandomValue(float from, float to)
@@ -24,37 +45,108 @@ public class ButtonManager : MonoBehaviour
 
     public void UpgradeDamage()
     {
-        int amount = (int)GenerateRandomValue(1, 5);
-        cannonStats.SetDamage(cannonStats.GetDamage() + amount);
-        Debug.Log($"Damage upgraded by {amount}. New damage: {cannonStats.GetDamage()}");
+        UpgradeStat("DamageUpgrade", () => cannonStats.SetDamage(cannonStats.GetDamage() + (int)upgradeAmounts["DamageUpgrade"]));
     }
 
     public void UpgradeHealth()
     {
-        int amount = (int)GenerateRandomValue(1, 5);
-        cannonStats.SetTempHealth(cannonStats.GetTempHealth() + amount);
-        cannonStats.SetMaxHealth(cannonStats.GetMaxHealth() + amount);
-        Debug.Log($"Health upgraded by {amount}. New temp health: {cannonStats.GetTempHealth()}, New max health: {cannonStats.GetMaxHealth()}");
+        UpgradeStat("HealthUpgrade", () =>
+        {
+            cannonStats.SetTempHealth(cannonStats.GetTempHealth() + (int)upgradeAmounts["HealthUpgrade"]);
+            cannonStats.SetMaxHealth(cannonStats.GetMaxHealth() + (int)upgradeAmounts["HealthUpgrade"]);
+        });
     }
 
     public void UpgradeProjectileSpeed()
     {
-        float amount = GenerateRandomValue(0.1f, 1.0f);
-        cannonStats.SetProjectileSpeed(cannonStats.GetProjectileSpeed() + amount);
-        Debug.Log($"Projectile speed upgraded by {amount}. New projectile speed: {cannonStats.GetProjectileSpeed()}");
+        UpgradeStat("ProjectileSpeedUpgrade", () => cannonStats.SetProjectileSpeed(cannonStats.GetProjectileSpeed() + upgradeAmounts["ProjectileSpeedUpgrade"]));
     }
 
     public void UpgradeRadius()
     {
-        float amount = GenerateRandomValue(0.5f, 2.5f);
-        cannonStats.SetRadius(cannonStats.GetRadius() + amount);
-        Debug.Log($"Radius upgraded by {amount}. New radius: {cannonStats.GetRadius()}");
+        UpgradeStat("RadiusUpgrade", () => cannonStats.SetRadius(cannonStats.GetRadius() + upgradeAmounts["RadiusUpgrade"]));
     }
 
     public void UpgradeShootingSpeed()
     {
-        float amount = GenerateRandomValue(0.1f, 0.5f);
-        cannonStats.SetShootingSpeed(cannonStats.GetShootingSpeed() + amount);
-        Debug.Log($"Shooting speed upgraded by {amount}. New shooting speed: {cannonStats.GetShootingSpeed()}");
+        // => is a lambda expression. It is a way to pass a function as a parameter.
+        // () means no parameters used in the lambda expression.
+        UpgradeStat("ShootingSpeedUpgrade", () => cannonStats.SetShootingSpeed(cannonStats.GetShootingSpeed() + upgradeAmounts["ShootingSpeedUpgrade"]));
+    }
+
+    private void UpgradeStat(string buttonName, System.Action upgradeAction)
+    {
+        // Executes the lambda expression that passed into it.
+        upgradeAction.Invoke();
+
+        Debug.Log($"{buttonName} upgraded by {upgradeAmounts[buttonName]}. New value: {GetStatValue(buttonName)}");
+        float newUpgradeAmount = GenerateRandomValue(GetMinValue(buttonName), GetMaxValue(buttonName));
+        upgradeAmounts[buttonName] = newUpgradeAmount;
+        UpdateValueText(buttonName, buttonTexts[buttonName], newUpgradeAmount);
+    }
+
+    private float GetStatValue(string buttonName)
+    {
+        switch (buttonName)
+        {
+            case "DamageUpgrade": return cannonStats.GetDamage();
+            case "HealthUpgrade": return cannonStats.GetTempHealth();
+            case "ProjectileSpeedUpgrade": return cannonStats.GetProjectileSpeed();
+            case "RadiusUpgrade": return cannonStats.GetRadius();
+            case "ShootingSpeedUpgrade": return cannonStats.GetShootingSpeed();
+            default: return 0;
+        }
+    }
+
+    private float GetMinValue(string buttonName)
+    {
+        switch (buttonName)
+        {
+            case "DamageUpgrade":
+            case "HealthUpgrade": return 1f;
+            case "RadiusUpgrade": return 0.5f;
+            case "ProjectileSpeedUpgrade": return 0.1f;
+            case "ShootingSpeedUpgrade": return 0.1f;
+            default: return 0;
+        }
+    }
+
+    private float GetMaxValue(string buttonName)
+    {
+        switch (buttonName)
+        {
+            case "DamageUpgrade":
+            case "HealthUpgrade": return 5f;
+            case "RadiusUpgrade": return 2.5f;
+            case "ProjectileSpeedUpgrade": return 1.0f;
+            case "ShootingSpeedUpgrade": return 0.5f;
+            default: return 0;
+        }
+    }
+
+    private void UpdateValueText(string buttonName, TextMeshProUGUI buttonText, float value)
+    {
+        if (buttonName == "DamageUpgrade" || buttonName == "HealthUpgrade")
+        {
+            buttonText.text = $"+{(int)value}";
+        }
+        else
+        {
+            buttonText.text = $"+{value:F1}";
+        }
+    }
+
+    private TextMeshProUGUI GetTextComponent(Button button)
+    {
+        TextMeshProUGUI[] textComponents = button.GetComponentsInChildren<TextMeshProUGUI>();
+        foreach (var textComponent in textComponents)
+        {
+            if (textComponent.name == "Value")
+            {
+                return textComponent;
+            }
+        }
+
+        return null;
     }
 }
